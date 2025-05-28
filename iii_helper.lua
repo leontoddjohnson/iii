13aa9b2dc69ef51e21589a30d302132f3975b1e3
Leon <johnson.leon.t@gmail.com>
2025-05-28 10:39:36 -0400

    set up main swons page

--- iii_helper.lua
+++ iii_helper.lua
@@ -4,9 +4,27 @@
 
 -- Draw a "point" between 1-1024 using a few LEDs on the arc.
 -- (cr: @tehn, from snows.lua)
-function point(n,x)
+local function point(n,x)
 	local c = x >> 4  -- bitwise shift right (divide by 2^4, round down)
 	arc_led_rel(n,c%64+1,15)
 	arc_led_rel(n,(c+1)%64+1,x%16)
 	arc_led_rel(n,(c+63)%64+1,15-(x%16))
+end
+
+-- ========================================================================== --
+-- ARC HELPER FUNCTIONS ON NORNS
+-- ========================================================================== --
+
+local music = require 'musicutil'
+
+-- Generate *one* octave of intervening intervals given a scale name,
+-- e.g. {2, 2, 1, 2, 2, 2, 1} for a major scale.
+-- See musicutil for full list of scale names.
+local function scale_intervals(scale)
+  scale = music.generate_scale(1, scale, 1) or {}
+  intervals = {}
+  for i = 2, #scale do
+    table.insert(intervals, scale[i] - scale[i - 1])
+  end
+  return intervals
 end
--- swons.lua
+++ swons.lua
@@ -1,15 +1,23 @@
 -- based on @tehn's snows.lua
 
 print("\n^______^")
-note = {}
-note[1] = {45,43,50}
-note[2] = {55,64}
-note[3] = {69,74,76,79}
-note[4] = {86,83,81,72,79}
+scale = {}
+scale[1] = { 2, 2, 1, 2, 2, 2, 1 }  -- major
+scale[2] = { 2, 1, 2, 2, 1, 2, 2 }  -- minor
+scale[3] = { 2, 2, 3, 2, 3 } -- major pentatonic
+scale[4] = { 3, 2, 2, 3, 2 } -- minor pentatonic
+scale[5] = { 2, 1, 2, 2, 2, 1, 2 }  -- dorian
+scale[6] = { 4, 1, 2, 4, 1 }  -- okinawa
 
-seq = {1,1,1,1}  -- seq[n] is the index of the current note in note[n]
-pos = {0,0,0,0}  -- position of each arc encoder
-sp = {0,0,0,0}  -- speed of each arc encoder
+notes = {}
+notes[1] = {45,43,50}
+notes[2] = {55,64}
+notes[3] = {69,74,76,79}
+notes[4] = {86,83,81,72,79}
+
+note = {1,1,1,1}  -- note[n] is the index of the current note in notes[n]
+position = {0,0,0,0}  -- position of each arc encoder
+speed = {0,0,0,0}  -- speed of each arc encoder
 
 REDRAW_FRAMERATE = 30
 
@@ -29,14 +37,14 @@ function tick()
 end
 
 function arc(n,d)
-	sp[n] = clamp(sp[n] + d,-32,32)
+	speed[n] = clamp(speed[n] + d,-32,32)
 end
 
 function arc_key(z)
-	for n=1,4 do sp[n] = 0 end
+	for n=1,4 do speed[n] = 0 end
 end
 
-m = metro.new(tick, 1000/REDRAW_FRAMERATE)
+m = metro.new(tick, 1000//REDRAW_FRAMERATE)
 
 -- ========================================================================== --
 -- UTILITY
@@ -45,34 +53,51 @@ m = metro.new(tick, 1000/REDRAW_FRAMERATE)
 -- draw and update the current position of ring `n`, also send MIDI.
 -- each encoder corresponds to the corresponding MIDI channel.
 function update_point(n)
-	pos[n] = pos[n] + sp[n]
+	position[n] = position[n] + speed[n]
 	ch = n  -- MIDI channel is the same as the arc ring number
 
 	-- passed the 0 point going in reverse
-	if pos[n] < 0 then
-		midi_note_off(note[n][seq[n]],127,ch)
-		seq[n] = ((seq[n] - 2) % #note[n]) + 1  -- previous note
-		midi_note_on(note[n][seq[n]],127,ch)
-		pos[n] = pos[n] % 1024
-		--ps("%d %d",n,seq[n])
+	if position[n] < 0 then
+		midi_note_off(notes[n][note[n]],127,ch)
+		note[n] = ((note[n] - 2) % #notes[n]) + 1  -- previous note
+		midi_note_on(notes[n][note[n]],127,ch)
+		position[n] = position[n] % 1024
+		--ps("%d %d",n,note[n])
 
 	-- passed the 0 point going forward
-	elseif pos[n] > 1023 then
-		midi_note_off(note[n][seq[n]],127,ch)
-		seq[n] = (seq[n] % #note[n]) + 1  -- next note
-		midi_note_on(note[n][seq[n]],127,ch)
-		pos[n] = pos[n] % 1024
-		--ps("%d %d",n,seq[n])
+	elseif position[n] > 1023 then
+		midi_note_off(notes[n][note[n]],127,ch)
+		note[n] = (note[n] % #notes[n]) + 1  -- next note
+		midi_note_on(notes[n][note[n]],127,ch)
+		position[n] = position[n] % 1024
+		--ps("%d %d",n,note[n])
 	end
 
-	point(n,pos[n])
+	point(n,position[n])
 end
 
 -- draw the note sequence for ring `n`.
--- Each note is represented by an LED, and the current note is highlighted.
+-- each note is represented by an LED, and the current note is highlighted.
 function draw_sequence(n)
-	for m=1,#note[n] do arc_led(n,32+m*2,1) end
-	arc_led_rel(n,32+seq[n]*2,9)
+	local next_note
+
+	-- define next note based on speed
+	if speed[n] < 0 then
+		next_note = ((note[n] - 2) % #notes[n]) + 1
+	else
+		next_note = (note[n] % #notes[n]) + 1
+	end
+
+	-- sprocket
+	for m=1,#notes[n] do
+		-- set past notes and future ones as dim
+		if m > next_note then arc_led(n, 2 * ((m - next_note) % 64) + 1, 1) end
+		if m < next_note then arc_led(n, 2 * ((m - next_note) % 32) + 1, 1) end
+
+		-- highlight next note
+		if m == next_note then arc_led(n, 1, 9) end
+	end
+
 end
 
 -- Draw a "point" between 1-1024 using a few LEDs on the arc.
